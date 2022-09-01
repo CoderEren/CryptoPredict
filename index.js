166587c3ff3d003c7aae1874110ea4ca202f7561
Eren Geridonmez <erengeridonmez@hotmail.com>
2022-09-01 22:17:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const Web3 = require("web3");
+const http = require("http");
+
+const url = 'https://rpc.ankr.com/eth_rinkeby'
+const port = process.env.PORT || 8000;
+
+const server = http.createServer((req, res) => {
+    const readstream = fs.createReadStream("./index.html");
+    if (req.url == "/node_modules/web3/dist/web3.min.js") {
+        fs.readFile("./node_modules/web3/dist/web3.min.js", "utf8", (err, file) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.write(file);
+                res.end();
+            }
+        });
+
+    } else if (req.url == "/node_modules/web3/dist/web3.min.js.map") {
+        fs.readFile("./node_modules/web3/dist/web3.min.js.map", "utf8", (err, file) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.write(file);
+                res.end();
+            }
+        });
+    } else if (req.url == "/") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        readstream.pipe(res);
+    } else if (req.url == "/bitcoin_header.svg") {
+        var img = fs.readFileSync('./bitcoin_header.svg');
+        res.writeHead(200, { "Content-type": "image/svg+xml" });
+        res.end(img, "binary");
+    } else if (req.url == "/cryptopredictlogo2.png") {
+        var img = fs.readFileSync('./cryptopredictlogo2.png');
+        res.writeHead(200, { "Content-type": "image/png" });
+        res.end(img, "binary");
+    } else if (req.url == "/cryptopredictlogo.png") {
+        var img = fs.readFileSync('./cryptopredictlogo.png');
+        res.writeHead(200, { "Content-type": "image/png" });
+        res.end(img, "binary");
+    } else if (req.url == "/favicon.png") {
+        var img = fs.readFileSync('./favicon.png');
+        res.writeHead(200, { "Content-type": "image/png" });
+        res.end(img, "binary");
+    } else {
+        res.write("Oops. This page does not exist!");
+        res.end();
+    }
+});
+
+console.log("Server started!");
+server.listen(port);
